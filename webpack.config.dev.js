e34f57b28408766806e2eddb4db30f3fbf02cad1
smartnix <smartnix@hotmail.com>
2022-07-29 19:27:15 -0500

    Proyecto listo para build and deply en netlify

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: '/src/index.js',
     output:{
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        // publicPath: "/",
+        filename: 'bundle.js'
     },
     resolve:{
-        extensions: ['.js', 'jsx'],
-        alias: {
-            '@components':path.resolve(__dirname, 'src/components'),
-            '@styles':path.resolve(__dirname, 'src/styles')
-        }
+        extensions: ['.js', 'jsx']
     },
+    mode:'development',
     module:{
         rules:[
             {
@@ -57,14 +50,15 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        }),
-        new CleanWebpackPlugin(),
-    ] ,
-    optimization:{
-        minimize:true,
-        minimizer:[
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
+        })
+    ]
+    ,
+    devServer:{
+        static: {
+            directory: path.join(__dirname,'dist'),
+          },
+        compress:true,
+        port:3002
+
     }
 }
